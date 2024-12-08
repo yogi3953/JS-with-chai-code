0e817a88d942ef8a2e84233282a5ce268341f70c
yogi3953 <144039670+yogi3953@users.noreply.github.com>
2024-12-08 08:18:29 +0000

    Numbers-Math

---
+++ 01-Basics/Strings.js
@@ -0,0 +1,27 @@
+//Strings are denoted by Single Quotes or Double Quotes.
+
+const Mname="Yogi";
+const repoCount=29;
+console.log(Mname+repoCount+"Value");
+console.log(`${Mname} and roll is ${repoCount}`);
+
+
+
+//Declaring String with String Object
+const myName=new String("Yogesh")
+console.log(Mname);
+console.log(myName)
+
+//Methods
+
+/*
+Go through MDN DOC
+*/
+
+
+
+
+
+
+
+
---
+++ 01-Basics/script.js
@@ -0,0 +1,34 @@
+console.log("Yogi");
+
+/*Primitve Data types
+Number
+String
+Boolea
+Null
+Undefined
+Symbol
+
+=>Stored by Value
+=>Immutabe
+=>When copied it is copies entire value not a reference
+=>We get copy of that value
+//Primitive values are stored into "stack" 
+*/
+
+
+/*
+Non-Primitive Datatype
+
+Objects
+Object Functions
+Arrays
+
+=>Stored by Reference
+=>Mutable
+=>When passed or shared the reference to that value is shared
+//when chnaged,changes the original value.
+//Non-Primitive values are stored into "Heap"
+=>We get reference of that value
+*/
+//Stack Memory
+//USed by all the primitive types of data
